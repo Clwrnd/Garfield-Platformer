164bc9d39d31d0043c09042abc20403feda2c922
Clwrnd <127970668+Clwrnd@users.noreply.github.com>
2024-09-10 21:53:44 +0200

    Grid-> a tester si le view marche bien

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -23,6 +23,16 @@ void Scene::registerAction(int inputKey, const std::string &actionName)
     actionMap[inputKey] = actionName;
 }
 
+size_t Scene::width() const
+{
+    return game_engine->getWindow().getSize().x;
+}
+
+size_t Scene::height() const
+{
+    return game_engine->getWindow().getSize().y;
+}
+
 void Scene::doAction(const Action &action)
 {
     sDoAction(action);
@@ -31,7 +41,7 @@ void Scene::doAction(const Action &action)
 void Scene::drawLine(const Vec2 &p1, const Vec2 &p2)
 {
     sf::RectangleShape rect;
-    rect.setFillColor(sf::Color::Red);
+    rect.setFillColor(sf::Color::White);
     rect.setPosition(sf::Vector2f(p1.x,p1.y));
     rect.setSize(sf::Vector2f(p2.x,p2.y));
     game_engine->getWindow().draw(rect); 
--- src/Scene_InGame.cpp
+++ src/Scene_InGame.cpp
@@ -6,6 +6,7 @@
 Scene_InGame::Scene_InGame(const std::string &levelPathVar, GameEngine *gameEngine)
     : Scene(gameEngine), levelPath(levelPathVar)
 {
+    
     init();
 }
 
@@ -29,11 +30,29 @@ void Scene_InGame::onEnd()
 
 void Scene_InGame::sRender() 
 {
-    game_engine->getWindow().clear(sf::Color(0, 162, 0));
+   
+
+   
+    
+
     if (drawGrid)
     {
-        std::cout<< "hey";
-        drawLine(Vec2{0, 0}, Vec2{1000,10});
+    game_engine->getWindow().clear(sf::Color(0, 162, 0));
+
+    float leftX = game_engine->getWindow().getView().getCenter().x - width() / 2;    
+    float rightX = leftX + width() + gridSize.x ;
+    float nextGrid = leftX - ((int)leftX % (int)gridSize.x);
+
+    for (float x = nextGrid; x < rightX; x+=gridSize.x)
+    {
+        drawLine(Vec2{x,0},Vec2{-1,height()});
+    }
+
+    for(float y = 0; y<height();y+=gridSize.y)
+    {
+        drawLine(Vec2{leftX,height()-y},Vec2{width(),-1});
+    }
+
     }
     game_engine->getWindow().display();
 }
